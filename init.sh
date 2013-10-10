261f1c77a410b3288f1fbcdcc4aab9196fdbcf4a
Mike Fotinakis <mike@fotinakis.com>
2013-10-10 15:06:45 -0700

    Fix bash eternal history\!

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ export HISTSIZE=
 export HISTTIMEFORMAT="[%F %T] "
 # Change the file location because other bash sessions truncate .bash_history file upon close.
 # http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
-HISTFILE=~/.bash_eternal_history
+export HISTFILE=~/.bash_eternal_history
 # Force prompt to write history after every command.
 # http://superuser.com/questions/20900/bash-history-loss
 PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"
